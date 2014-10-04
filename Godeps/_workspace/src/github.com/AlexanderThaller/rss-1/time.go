7f46f98ce679f15b7dbfa7b783a3378ab0f3efcf
Alexander Thaller <alexander@thaller.ws>
2014-10-04 15:48:12 +0200

    updated dependencies.

--- Godeps/_workspace/src/github.com/AlexanderThaller/rss-1/time.go
+++ Godeps/_workspace/src/github.com/AlexanderThaller/rss-1/time.go
@@ -3,35 +3,40 @@ package rss
 import (
 	"strings"
 	"time"
+
+	"github.com/jinzhu/now"
+	"github.com/juju/errgo"
 )
 
-func parseTime(s string) (time.Time, error) {
-	formats := []string{
-		"Mon, _2 Jan 2006 15:04:05 MST",
-		"Mon, _2 Jan 2006 15:04:05 -0700",
-		time.ANSIC,
-		time.UnixDate,
-		time.RubyDate,
-		time.RFC822,
-		time.RFC822Z,
-		time.RFC850,
-		time.RFC1123,
-		time.RFC1123Z,
-		time.RFC3339,
-		time.RFC3339Nano,
+func init() {
+	now.TimeFormats = append(now.TimeFormats, time.ANSIC)
+	now.TimeFormats = append(now.TimeFormats, time.UnixDate)
+	now.TimeFormats = append(now.TimeFormats, time.RubyDate)
+	now.TimeFormats = append(now.TimeFormats, time.RFC822)
+	now.TimeFormats = append(now.TimeFormats, time.RFC822Z)
+	now.TimeFormats = append(now.TimeFormats, time.RFC850)
+	now.TimeFormats = append(now.TimeFormats, time.RFC1123)
+	now.TimeFormats = append(now.TimeFormats, time.RFC1123Z)
+	now.TimeFormats = append(now.TimeFormats, time.RFC3339)
+	now.TimeFormats = append(now.TimeFormats, time.RFC3339Nano)
+	now.TimeFormats = append(now.TimeFormats, "Mon, _2 Jan 2006 15:04:05 MST")
+	now.TimeFormats = append(now.TimeFormats, "Mon, _2 Jan 2006 15:04:05 -0700")
+	now.TimeFormats = append(now.TimeFormats, "Mon, _2 January 2006 15:04:05 MST")
+	now.TimeFormats = append(now.TimeFormats, "Mon, _2 January 2006 15:04:05 -0700")
+	now.TimeFormats = append(now.TimeFormats, "Mon, _2 Jan 06 15:04:05 -0700")
+	now.TimeFormats = append(now.TimeFormats, "_2 Jan 2006 15:04:05 -0700")
+}
+
+func parseTime(source string) (time.Time, error) {
+	source = strings.TrimSpace(source)
+	source = strings.Replace(source, "Tues", "Tue", 1)
+	source = strings.Replace(source, "Thur", "Thu", 1)
+	source = strings.Replace(source, "Thus", "Thu", 1)
+
+	t, err := now.Parse(source)
+	if err != nil {
+		return time.Time{}, errgo.New(err.Error())
 	}
 
-	s = strings.TrimSpace(s)
-
-	var e error
-	var t time.Time
-
-	for _, format := range formats {
-		t, e = time.Parse(format, s)
-		if e == nil {
-			return t, e
-		}
-	}
-
-	return time.Time{}, e
+	return t, nil
 }
